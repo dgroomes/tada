1c5e2dbcb6ee10ff5f7556b3dd8d00d0f5937925
David Groomes <dwgroomes@gmail.com>
2017-04-10 23:41:11 -0500

    upgrade to new way of doing instrumentation tests: Android Testing Support Library (ATSL)

---
+++ tada/src/androidTest/java/us/mn/dgtc/tada/CustomRunner.java
@@ -0,0 +1,7 @@
+package us.mn.dgtc.tada;
+
+import android.support.test.runner.AndroidJUnitRunner;
+
+
+public class CustomRunner extends AndroidJUnitRunner {
+}
---
+++ tada/src/androidTest/java/us/mn/dgtc/tada/InstrumentedTest.java
@@ -0,0 +1,17 @@
+package us.mn.dgtc.tada;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class InstrumentedTest {
+
+    @Test
+    public void testOne() throws Exception {
+        assertTrue(true);
+    }
+}
